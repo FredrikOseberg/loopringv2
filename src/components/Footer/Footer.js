019be068e3907310de1bf38f050f318d9ac791d6
FredrikOseberg <fredrik.no@gmail.com>
2017-12-11 18:42:24 +1000

    Add github pages

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,6 +8,33 @@ const Footer = () => (
 			<img src={Loopring} alt="Loopring" />
 			<h1>Loopring</h1>
 		</div>
+		<div className="footer--content--section">
+			<div className="footer--container">
+				<div className="footer--content--links">
+					<h4>Loopring Foundation. All rights reserved.</h4>
+				</div>
+				<div className="footer--content--social">
+					<a href="#" className="footer--social--link">
+						<i className="fab fa-github-alt" />
+					</a>
+
+					<a href="#" className="footer--social--link">
+						<i className="fab fa-telegram" />
+					</a>
+
+					<a href="#" className="footer--social--link">
+						<i className="fas fa-envelope" />
+					</a>
+
+					<a href="#" className="footer--social--link">
+						<i className="fab fa-twitter" />
+					</a>
+					<a href="#" className="footer--social--link">
+						<i className="fab fa-medium-m" />
+					</a>
+				</div>
+			</div>
+		</div>
 	</section>
 );
 
